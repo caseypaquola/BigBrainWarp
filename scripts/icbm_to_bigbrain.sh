8fbf42d243997fe6650399b5708bbc4d765c6986
caseypaquola <34452058+caseypaquola@users.noreply.github.com>
2020-08-13 18:34:40 -0400

    Update icbm_to_bigbrain.sh

--- scripts/icbm_to_bigbrain.sh
+++ scripts/icbm_to_bigbrain.sh
@@ -6,7 +6,7 @@
 
 fileName=$1 		# name of input file without extension. Must be a .mnc file. (Try nii2mnc if your data is nifti)
 			# output is in form ${fileName}_bigbrain.mnc
-gitRepo=$2		# /path/to/BigBrainWarp/
-mincresample -clobber -transformation ${gitRepo}/xfms/BigBrain-to-ICBM2009sym-nonlin.xfm -invert_transformation -tfm_input_sampling -nearest_neighbour ${fileName}.mnc ${fileName}_nl.mnc
-mincresample -clobber -transformation ${gitRepo}/xfms/bigbrain_to_icbm2009b_nl.xfm -invert_transformation -tfm_input_sampling -nearest_neighbour ${fileName}_nl.mnc ${fileName}_nl_nl.mnc
-mincresample -clobber -transformation ${gitRepo}/xfms/bigbrain_to_icbm2009b_lin.xfm -invert_transformation -tfm_input_sampling -nearest_neighbour ${fileName}_nl_nl.mnc ${fileName}_bigbrain.mnc
+bbwDir=$2		# /path/to/BigBrainWarp/
+mincresample -clobber -transformation ${bbwDir}/xfms/BigBrain-to-ICBM2009sym-nonlin.xfm -invert_transformation -tfm_input_sampling -nearest_neighbour ${fileName}.mnc ${fileName}_nl.mnc
+mincresample -clobber -transformation ${bbwDir}/xfms/bigbrain_to_icbm2009b_nl.xfm -invert_transformation -tfm_input_sampling -nearest_neighbour ${fileName}_nl.mnc ${fileName}_nl_nl.mnc
+mincresample -clobber -transformation ${bbwDir}/xfms/bigbrain_to_icbm2009b_lin.xfm -invert_transformation -tfm_input_sampling -nearest_neighbour ${fileName}_nl_nl.mnc ${fileName}_bigbrain.mnc
