a9c6d834212802757a28f3bfc76c11c31138e250
Casey Paquola <casey.paquola@gmail.com>
2020-08-14 14:00:13 -0400

    parcellations and iEEG channels

--- scripts/fsaverage_to_bigbrain.sh
+++ scripts/fsaverage_to_bigbrain.sh
@@ -9,23 +9,33 @@ outName=$4
 bbwDir=$5
 cbigDir=$6
 
-export MATLABPATH=$MATLABPATH:$bbwDir
+. /etc/fsl/5.0/fsl.sh
+
+# use Wu et al., transformation from fsaverage to mni152
+export MATLABPATH=$MATLABPATH:$bbwDir/scripts
 matlab19b -r 'wrapper_fsaverage2mni("'${lh_input}'", "'${rh_input}'", "'${interp}'", "'${outName}'", "'${bbwDir}'", "'${cbigDir}'"); quit'
 nii2mnc ${outName}_mni152.nii ${outName}_mni152.mnc
 rm -rf ${outName}_mni152.nii
 
-sh $bbwDir/scripts/icbm_to_bigbrain.sh ${outName}_mni152 $bbwDir
-mnc2nii ${outName}_mni152_bigbrain.mnc ${outName}_bigbrain.nii
-rm -rf ${outName}_mni152_bigbrain.mnc
+# precise interpolation method for minc
+if [ ${interp} = linear ]; then
+	mnc_interp = linear
+elif [ ${interp} = nearest ] ; then
+	mnc_interp=nearest_neighbour
+fi
+
+# transform from mni152 to bigbrain volume space
+export PATH=/data_/mica1/01_programs/minc2/:$PATH
+sh $bbwDir/scripts/icbm_to_bigbrain.sh ${outName}_mni152 $bbwDir $mnc_interp
+mnc2nii -clobber ${outName}_mni152_bigbrain.mnc ${outName}_bigbrain.nii
+
+# dilate the volume in bigbrain space to ensure the whole cortical ribbon is covered
 fslmaths ${outName}_bigbrain.nii -dilD -dilD -dilD -dilD -dilD ${outName}_bigbrain_dilated.nii
+gunzip ${outName}_bigbrain_dilated.nii.gz
 nii2mnc ${outName}_bigbrain_dilated.nii ${outName}_bigbrain_dilated.mnc
 
-if [ ${interp} = linear ]; then
-	voe_interp = linear
-elif [ ${interp} = nearest ]
-	voe_interp=nearest_neighbour
-fi
-volume_object_evaluate -${voe_interp} ${outName}_bigbrain_dilated.mnc $bbwDir/bigbrain_surfaces/equivolumetric/lh.9.obj ${outName}_bigbrain_lh.txt
-volume_object_evaluate -${voe_interp} ${outName}_bigbrain_dilated.mnc $bbwDir/bigbrain_surfaces/equivolumetric/rh.9.obj ${outName}_bigbrain_rh.txt
+# sample parcellation values from the cortical midsurface
+volume_object_evaluate -${mnc_interp} ${outName}_bigbrain_dilated.mnc $bbwDir/bigbrain_surfaces/equivolumetric/lh.9.obj ${outName}_bigbrain_lh.txt
+volume_object_evaluate -${mnc_interp} ${outName}_bigbrain_dilated.mnc $bbwDir/bigbrain_surfaces/equivolumetric/rh.9.obj ${outName}_bigbrain_rh.txt
 
 
--- scripts/transform_parcellations.sh
+++ scripts/transform_parcellations.sh
@@ -1,26 +1,26 @@
 #/bin/bash
 
+parc=$1
+
 bbwDir=/data_/mica1/03_projects/casey/BigBrainWarp/
 cbigDir=/data_/mica1/03_projects/casey/CBIG-master/
 pipeDir=/data_/mica1/03_projects/casey/micapipe/
 
 export PATH=/data_/mica1/01_programs/minc2/:$PATH
+export FREESURFER_HOME=/data_/mica1/03_projects/casey/freesurfer
+source $FREESURFER_HOME/SetUpFreeSurfer.sh
 
-for parc in vosdewael_100 vosdewael_200 vosdewael_300 vosdewael_400 schaefer_100 schaefer_200 schaefer_300 schaefer_400 glasser_360 aparc ; do
-
-	for hemi in lh rh ; do
-		mri_surf2surf --srcsubject fsaverage5 --trgsubject fsaverage \
-		--hemi $hemi \
-		--sval-annot $pipeDir/parcellations/${hemi}.${parc}_fsa5.annot \
-		--tval $bbwDir/maps/fsaverage_space/parcellations/${hemi}.${parc}.annot
-	done
-
-	sh fsaverage_to_bigbrain $bbwDir/maps/fsaverage_space/parcellations/lh.${parc}.annot \
-		$bbwDir/maps/fsaverage_space/parcellations/rh.${parc}.annot \
-		nearest \
-		$bbwDir/maps/mni152_space/parcellations/${parc} \
-		$bbwDir $cbigDir
+for hemi in lh rh ; do
+	mri_surf2surf --srcsubject fsaverage5 --trgsubject fsaverage \
+	--hemi $hemi \
+	--sval-annot $pipeDir/parcellations/${hemi}.${parc}_fsa5.annot \
+	--tval $bbwDir/maps/fsaverage_space/parcellations/${hemi}.${parc}.annot
 done
+
+sh $bbwDir/scripts/fsaverage_to_bigbrain.sh $bbwDir/maps/fsaverage_space/parcellations/lh.${parc}.annot $bbwDir/maps/fsaverage_space/parcellations/rh.${parc}.annot nearest $bbwDir/maps/bigbrain_space/parcellations/${parc} $bbwDir $cbigDir
+
+rm -rf $bbwDir/maps/bigbrain_space/${parc}*.nii
+rm -rf $bbwDir/maps/bigbrain_space/${parc}*.mnc
 
 
 
