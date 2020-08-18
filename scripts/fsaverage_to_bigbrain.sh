f81deaddf2b629ca7687cd86a5f980744c72e5bc
caseypaquola <34452058+caseypaquola@users.noreply.github.com>
2020-08-18 16:49:36 -0400

    workflow

--- scripts/fsaverage_to_bigbrain.sh
+++ scripts/fsaverage_to_bigbrain.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# perform surface to volume mapping then nonlinear transformation from fsaverage to BigBrain space
+# perform surface to volume mapping then nonlinear transformation from fsaverage to BigBrain surface
 # written by Casey Paquola @ MICA, MNI, 2020*
 
 lh_input=$1
@@ -9,13 +9,11 @@ outName=$4
 bbwDir=$5
 cbigDir=$6
 
-. /etc/fsl/5.0/fsl.sh
-
 # use Wu et al., transformation from fsaverage to mni152
 export MATLABPATH=$MATLABPATH:$bbwDir/scripts
-#matlab19b -r 'wrapper_fsaverage2mni("'${lh_input}'", "'${rh_input}'", "'${interp}'", "'${outName}'", "'${bbwDir}'", "'${cbigDir}'"); quit'
-#nii2mnc ${outName}_mni152.nii ${outName}_mni152.mnc
-#rm -rf ${outName}_mni152.nii
+matlab19b -r 'wrapper_fsaverage2mni("'${lh_input}'", "'${rh_input}'", "'${interp}'", "'${outName}'", "'${bbwDir}'", "'${cbigDir}'"); quit'
+nii2mnc ${outName}_mni152.nii ${outName}_mni152.mnc
+rm -rf ${outName}_mni152.nii
 
 # precise interpolation method for minc
 if [ ${interp} = linear ]; then
@@ -30,11 +28,8 @@ fi
 
 # transform from mni152 to bigbrain volume space
 export PATH=/data_/mica1/01_programs/minc2/:$PATH
-#sh $bbwDir/scripts/icbm_to_bigbrain.sh ${outName}_mni152 $bbwDir $mnc_interp
-#mincresample -clobber -transformation ${bbwDir}/xfms/BigBrain-to-ICBM2009sym-nonlin.xfm -invert_transformation -tfm_input_sampling -${mnc_interp} ${outName}_mni152.mnc ${outName}_mni152_nl.mnc
-#mincresample -clobber -transformation ${bbwDir}/xfms/bigbrain_to_icbm2009b_nl.xfm -invert_transformation -tfm_input_sampling -${mnc_interp} ${outName}_mni152_nl.mnc ${outName}_mni152_nl_nl.mnc
-#mincresample -clobber -transformation ${bbwDir}/xfms/bigbrain_to_icbm2009b_lin.xfm -invert_transformation -tfm_input_sampling -${mnc_interp} ${outName}_mni152_nl_nl.mnc ${outName}_mni152_bigbrain.mnc
-#mnc2nii ${outName}_mni152_bigbrain.mnc ${outName}_bigbrain.nii
+mincresample -clobber -transformation ${bbwDir}/xfms/BigBrain-to-ICBM2009sym-nonlin.xfm -invert_transformation -tfm_input_sampling -${mnc_interp} ${outName}_mni152.mnc ${outName}_bigbrain.mnc
+mnc2nii ${outName}_bigbrain.mnc ${outName}_bigbrain.nii
 
 # dilate the volume in bigbrain space to ensure the whole cortical ribbon is covered
 fslmaths ${outName}_bigbrain.nii -abs -thr 0.001 -bin -mul ${outName}_bigbrain.nii ${outName}_bigbrain_thresh.nii
@@ -43,7 +38,7 @@ gunzip ${outName}_bigbrain_dilated.nii.gz
 nii2mnc ${outName}_bigbrain_dilated.nii ${outName}_bigbrain_dilated.mnc
 
 # sample parcellation values from the cortical midsurface
-volume_object_evaluate -$voe_linear ${outName}_bigbrain_dilated.mnc $bbwDir/bigbrain_surfaces/equivolumetric/lh.9.obj ${outName}_bigbrain_lh.txt
-volume_object_evaluate -$voe_linear ${outName}_bigbrain_dilated.mnc $bbwDir/bigbrain_surfaces/equivolumetric/rh.9.obj ${outName}_bigbrain_rh.txt
+volume_object_evaluate -$voe_interp ${outName}_bigbrain_dilated.mnc $bbwDir/bigbrainsym_surfaces/equivolumetric/lh.9.obj ${outName}_bigbrain_lh.txt
+volume_object_evaluate -$voe_interp ${outName}_bigbrain_dilated.mnc $bbwDir/bigbrainsym_surfaces/equivolumetric/rh.9.obj ${outName}_bigbrain_rh.txt
 
 
--- scripts/icbm_to_bigbrain.sh
+++ scripts/icbm_to_bigbrain.sh
@@ -6,11 +6,17 @@
 
 fileName=$1 		# name of input file without extension. Must be a .mnc file. (Try nii2mnc if your data is nifti)
 			# output is in form ${fileName}_bigbrain.mnc
-bbwDir=$2		# /path/to/BigBrainWarp/
-interp=$3		# linear (smooth data) or nearest_neighbour (discrete data)
+bbSpace=$2 		# which bigbrain space to output to: "histological" or "sym"
+interp=$3		# "linear" (smooth data) or "nearest_neighbour" (discrete data)
+bbwDir=$4		# /path/to/BigBrainWarp/
 
-# have to define mncDir again for some reason
+
 /data_/mica1/01_programs/minc2/mincresample -transformation ${bbwDir}/xfms/BigBrain-to-ICBM2009sym-nonlin.xfm -invert_transformation -tfm_input_sampling -$interp ${fileName}.mnc ${fileName}_nl.mnc
-/data_/mica1/01_programs/minc2/mincresample -transformation ${bbwDir}/xfms/bigbrain_to_icbm2009b_nl.xfm -invert_transformation -tfm_input_sampling -$interp ${fileName}_nl.mnc ${fileName}_nl_nl.mnc
-/data_/mica1/01_programs/minc2/mincresample -transformation ${bbwDir}/xfms/bigbrain_to_icbm2009b_lin.xfm -invert_transformation -tfm_input_sampling -$interp ${fileName}_nl_nl.mnc ${fileName}_bigbrain.mnc
+if [[ $bbSpace=histological ]] ; then
+	/data_/mica1/01_programs/minc2/mincresample -transformation ${bbwDir}/xfms/bigbrain_to_icbm2009b_nl.xfm -invert_transformation -tfm_input_sampling -$interp ${fileName}_nl.mnc ${fileName}_nl_nl.mnc
+	/data_/mica1/01_programs/minc2/mincresample -transformation ${bbwDir}/xfms/bigbrain_to_icbm2009b_lin.xfm -invert_transformation -tfm_input_sampling -$interp ${fileName}_nl_nl.mnc ${fileName}_bigbrain.mnc
+else
+	mv ${fileName}_nl.mnc ${fileName}_bigbrain.mnc
+end
+rm -rf *nl.mnc
 
