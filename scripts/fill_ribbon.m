7bc988c39111e6b5f9ca762d75b927586a86c70d
caseypaquola <34452058+caseypaquola@users.noreply.github.com>
2020-08-20 11:35:58 -0400

    Create fill_ribbon.m

---
+++ scripts/fill_ribbon.m
@@ -0,0 +1,21 @@
+function fill_ribbon(lh_surf, rh_surf, template_vol, out_name, bbwDir)
+
+% add path
+addpath(genpath(bbwDir))
+
+% load surface
+S = SurfStatAvSurf({lh_surf, rh_surf});
+
+% load template volume
+vol = niftiread(template_vol);
+info = niftiinfo(template_vol);
+
+% populate new volume with vertex assignment
+new_vol = zeros(size(vol));
+for ii = 1:length(S.coord)
+    vol_coord = round((S.coord(:,ii) - info.Transform.T(4,1:3)')/info.Transform.T(1,1)); % transformation from surface to volume
+    new_vol(vol_coord(1), vol_coord(2), vol_coord(3)) = ii; 
+end
+
+info.Filename = out_name;
+niftiwrite(new_vol, out_name)
