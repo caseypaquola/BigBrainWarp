fe3e7761fe262f427e47dac6a3d6b523c4ec83a3
Casey Paquola <casey.paquola@gmail.com>
2020-08-20 09:50:31 -0400

    updated gradient mapping

--- scripts/bigbrain_to_icbm.sh
+++ scripts/bigbrain_to_icbm.sh
@@ -6,6 +6,7 @@
 fileName=$1 		# input file name without extension. Must be a .mnc file. (Try nii2mnc if its a nifti)
 			# output will be in the form ${fileName}_bigbrain.mnc
 bbwDir=$2 		# /path/to/BigBrainWarp/
-mincresample -clobber -transformation ${bbwDir}/xfms/bigbrain_to_icbm2009b_lin.xfm -use_input_sampling -nearest_neighbour ${fileName}.mnc ${fileName}_lin.mnc
-mincresample -clobber -transformation ${bbwDir}/xfms/bigbrain_to_icbm2009b_nl.xfm -use_input_sampling -nearest_neighbour ${fileName}_lin.mnc ${fileName}_lin_nl.mnc
-mincresample -clobber -transformation ${bbwDir}/xfms/BigBrain-to-ICBM2009sym-nonlin.xfm -use_input_sampling -nearest_neighbour ${fileName}_lin_nl.mnc ${fileName}_icbm.mnc
+interp=$3
+mincresample -clobber -transformation ${bbwDir}/xfms/bigbrain_to_icbm2009b_lin.xfm -tfm_input_sampling -${interp} ${fileName}.mnc ${fileName}_lin.mnc
+mincresample -clobber -transformation ${bbwDir}/xfms/bigbrain_to_icbm2009b_nl.xfm -tfm_input_sampling -${interp} ${fileName}_lin.mnc ${fileName}_lin_nl.mnc
+mincresample -clobber -transformation ${bbwDir}/xfms/BigBrain-to-ICBM2009sym-nonlin.xfm -tfm_input_sampling -${interp} ${fileName}_lin_nl.mnc ${fileName}_icbm.mnc
