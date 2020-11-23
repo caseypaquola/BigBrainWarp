a7efe2c6a6f3c528152c775aca8b91089e13c493
Casey Paquola <casey.paquola@gmail.com>
2020-11-23 16:15:32 -0500

    updating scripts

--- scripts/bigbrainsurf_to_fsaverage.sh
+++ scripts/bigbrainsurf_to_fsaverage.sh
@@ -10,30 +10,45 @@ interp=$6		# "linear" (smooth data) or "nearest_neighbour" (discrete data)
 workDir=$7 		# working directory
 cleanup=$8 		# "y" to remove intermediate files, "n" to keep
 
-% create workDir if is doesn't already exist
+# create workDir if is doesn't already exist
 [[ -d $workDir ]] || mkdir -p $workDir
 
-% get core of lh_input for file
+# get core of lh_input for file
 fileName=$(basename -- "$lh_input")
 fileName="${fileName%.*}"
 fileName="${fileName##*.}"
 
-% fill the ribbon
+# precise interpolation method for minc
+if [ ${interp} = linear ]; then
+	mnc_interp=trilinear
+	dil=dilM # dilation of mean
+elif [ ${interp} = nearest ] ; then
+	mnc_interp=nearest_neighbour
+	dil=dilD # dilation of mode
+fi
+
+# fill the ribbon
 outName=$workDir/${fileName}.mnc
 matlab -r 'fill_ribbon("'${lhInput}'","'${rhInput}'","'${lhSurf}'","'${rhSurf}'","'${icbmTemplate}'","'${outName}'","'${bbwDir}'"); quit'
 
-
-% transformation in volume space
+# transformation in volume space
 echo "transform to icbm"
-mincresample -clobber -transformation ${bbwDir}/xfms/BigBrain-to-ICBM2009sym-nonlin.xfm -tfm_input_sampling -like "${icbmTemplate}" -"$interp" "$workDir"/${fileName}.mnc "$workDir"/${fileName}_icbm.mnc
+mincresample -clobber -transformation ${bbwDir}/xfms/BigBrain-to-ICBM2009sym-nonlin.xfm -tfm_input_sampling -like "${icbmTemplate}" -"$mnc_interp" "$workDir"/${fileName}.mnc "$workDir"/${fileName}_icbm.mnc
 
-% transformation to surface space
-matlab -r 'wrapper_fsaverage2mni("'${${fileName}_icbm.mnc}'", "'${interp}'", "'${fileName}'", "'${bbwDir}'", "'${cbigDir}'"); quit'
+# convert to nifti and perform dilation
+mnc2nii "$workDir"/${fileName}_icbm.mnc "$workDir"/${fileName}_icbm.nii
+rm "$workDir"/${fileName}_icbm.mnc
+fslmaths "$workDir"/${fileName}_icbm.nii -$dil -$dil -$dil -$dil -$dil "$workDir"/${fileName}_icbm_dilated.nii
 
-% clean up if selected
+# transformation to surface space
+inputVol="$workDir"/${fileName}_icbm_dilated.nii
+matlab -r 'wrapper_mni2fsaverage("'${inputVol}'", "'${interp}'", "'${fileName}'", "'${bbwDir}'", "'${cbigDir}'"); quit'
+
+# clean up if selected
 if [[ "$cleanup" == "y" ]] ; then
 	rm "$workDir"/"$fileName"_lin*
 	if [[ "$extension" != "mnc" ]] ; then
 		rm "$workDir"/${fileName}_icbm.mnc
+		rm "$workDir"/${fileName}*.nii
 	fi
 fi
