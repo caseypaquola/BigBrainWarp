29337b246f41a6e182db06e694d8cd7980296bc4
Casey Paquola <casey.paquola@gmail.com>
2020-11-23 13:27:39 -0500

    catch up to head

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,20 +4,14 @@
 bbwDir=/data_/mica1/03_projects/casey/BigBrainWarp/
 cbigDir=/data_/mica1/03_projects/casey/CBIG-master/
 icbmTemplate=$bbwDir/spaces/mni152/mni_icbm152_nlin_asym_09c_mask.mnc
-<<<<<<< HEAD
 mnc2Path=/data_/mica1/01_programs/minc2/
-=======
->>>>>>> 47ed142cd313b97aefdf76a2775b6ad65fec8c88
 
 # download template
 if [[ ! -f $icbmTemplate ]] ; then
 	cd $bbwDir/spaces/mni152/
 	wget http://www.bic.mni.mcgill.ca/~vfonov/icbm/2009/mni_icbm152_nlin_asym_09c_minc2.zip
 	unzip mni_icbm152_nlin_asym_09c_minc2.zip
-<<<<<<< HEAD
 	rm mni_icbm152_nlin_asym_09c_minc2.zip
-=======
->>>>>>> 47ed142cd313b97aefdf76a2775b6ad65fec8c88
 fi
 
 # download nonlinear transformation matrices (note: large files)
@@ -41,11 +35,8 @@ fi
 
 
 # add to paths
-<<<<<<< HEAD
 export PATH=$bbwDir/scripts/:$mnc2Path:$PATH
-=======
 export PATH=$bbwDir/scripts/:$PATH
->>>>>>> 47ed142cd313b97aefdf76a2775b6ad65fec8c88
 export MATLABPATH=$bbwDir/scripts/:$MATLABPATH
 
 
