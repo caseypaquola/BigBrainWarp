896d2787e836928c08bcea05172dbf4fd3edcb87
Casey Paquola <casey.paquola@gmail.com>
2020-11-23 16:04:59 -0500

    updating scripts

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,10 +3,10 @@
 # set up variables - change for your environment
 bbwDir=/data_/mica1/03_projects/casey/BigBrainWarp/
 cbigDir=/data_/mica1/03_projects/casey/CBIG-master/
-icbmTemplate=$bbwDir/spaces/mni152/mni_icbm152_nlin_asym_09c_mask.mnc
 mnc2Path=/data_/mica1/01_programs/minc2/
 
-# download template
+# set template and download if not already there
+icbmTemplate=$bbwDir/spaces/mni152/mni_icbm152_nlin_asym_09c_mask.mnc
 if [[ ! -f $icbmTemplate ]] ; then
 	cd $bbwDir/spaces/mni152/
 	wget http://www.bic.mni.mcgill.ca/~vfonov/icbm/2009/mni_icbm152_nlin_asym_09c_minc2.zip
@@ -28,11 +28,18 @@ if [[ ! -f $bbwDir/xfms/BigBrain-to-ICBM2009sym-nonlin_grid_2.mnc ]] ; then
 	rm mni_PD25_20190708_minc2.zip
 fi
 
+# download surfstat to dependencies if not already there
+if [[ ! -d $bbwDir/surfstat/ ]] ; then
+	cd $bbwDir/dependencies/
+	wget https://www.math.mcgill.ca/keith/surfstat/surfstat.zip
+	unzip surfstat.zip -d surfstat
+	rm -f surfstat.zip
+fi
+
 # make git ignore
 if [[ ! -f $bbwDir/.gitignore ]] ; then
 	cp $bbwDir/template_gitignore.txt $bbwDir/.gitignore
 fi
-
 
 # add to paths
 export PATH=$bbwDir/scripts/:$mnc2Path:$PATH
