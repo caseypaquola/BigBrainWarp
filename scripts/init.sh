f57cc3bb6c12deccb4fab13018d322c0cb926075
Casey Paquola <casey.paquola@gmail.com>
2020-12-03 17:13:35 -0500

    updating init.sh with BigBrain templates

--- scripts/init.sh
+++ scripts/init.sh
@@ -30,13 +30,23 @@ fi
 
 # download nonlinear transformation for BigBrainHist       
 if [[ ! -f $bbwDir/xfms/BigBrainHist-to-ICBM2009sym-nonlin_grid_2.mnc ]] ; then                                                                              
-	cd $bbwDir/xfms/                                                                                                                                                                                       
-	wget -O BigBrainHist-to-ICBM2009sym-nonlin.xfm https://osf.io/mr4gn/download                                                                                                                           
-	wget -O BigBrainHist-to-ICBM2009sym-nonlin_grid_0.mnc https://osf.io/a8ks2/download                                                                                                                    
+	cd $bbwDir/xfms/                                                                                                                                              
+	wget -O BigBrainHist-to-ICBM2009sym-nonlin.xfm https://osf.io/mr4gn/download                                             
+	wget -O BigBrainHist-to-ICBM2009sym-nonlin_grid_0.mnc https://osf.io/a8ks2/download                                                         
 	wget -O BigBrainHist-to-ICBM2009sym-nonlin_grid_1.mnc https://osf.io/83rgw/download
 	wget -O BigBrainHist-to-ICBM2009sym-nonlin_grid_2.mnc https://osf.io/59c7v/download
  	wget -O BigBrainHist-to-ICBM2009sym-nonlin_grid_3.mnc https://osf.io/j8q3e/download
-fi  
+fi
+
+# download bigbrain templates
+if [[ ! -f $bbwDir/spaces/bigbrainsym/full8_400um_2009b_sym.mnc ]] ; then
+	cd $bbwDir/spaces/bigbrainsym/
+	wget ftp://bigbrain.loris.ca/BigBrainRelease.2015/3D_Volumes/MNI-ICBM152_Space/full8_400um_2009b_sym.mnc
+fi
+if [[ ! -f $bbwDir/spaces/bigbrain/full8_400um_optbal.mnc ]] ; then
+	cd $bbwDir/spaces/bigbrain/
+	wget ftp://bigbrain.loris.ca/BigBrainRelease.2015/3D_Volumes/Histological_Space/full8_400um_optbal.mnc
+fi
 
 # download surfstat to dependencies if not already there
 if [[ ! -d $bbwDir/dependencies/surfstat/ ]] ; then
