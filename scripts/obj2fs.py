7f532c0bfa93d93ecc3d0e1c914e0273e6d5864c
caseypaquola <34452058+caseypaquola@users.noreply.github.com>
2021-01-29 18:07:49 +0100

    vis scripts

---
+++ scripts/obj2fs.py
@@ -0,0 +1,13 @@
+import numpy as np
+import io_mesh as io
+import os
+import nibabel
+import sys
+
+obj=str(sys.argv[1])
+print(obj)
+outName=str(sys.argv[2])
+print(outName)
+
+mesh = io.read_obj(obj)
+nibabel.freesurfer.io.write_geometry(outName, coords, faces)
