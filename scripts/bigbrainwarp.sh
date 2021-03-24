6cf1b821862c9b2967924f733ee3e918c76a0618
caseypaquola <34452058+caseypaquola@users.noreply.github.com>
2021-03-24 16:58:17 +0100

    Update bigbrainwarp.sh

--- scripts/bigbrainwarp.sh
+++ scripts/bigbrainwarp.sh
@@ -19,12 +19,12 @@ echo -e "
 \033[38;5;141mVOLUME-BASED ARGUMENTS:\033[0m
 \t\033[38;5;197m-in\033[0m               : (required) full path to input data. Must be whole brain. Can be mnc, nii or nii.gz
 \t\033[38;5;197m-interp\033[0m           : (optional) interpolation method, can be trilinear, tricubic, nearest or sinc. Default is trilinear. 
-\t\033[38;5;197m-out_name\033[0m         : (optional) prefix for output files. Default is prefix of input file. Output will be in the form $wd/${out_name}_${out_space}
+\t\033[38;5;197m-out_name\033[0m         : (optional) prefix for output files. Default is prefix of input file. Output will be in the form {wd}/{out_name}_{out_space}
 
 \033[38;5;141mSURFACE-BASED ARGUMENTS:\033[0m
 \t\033[38;5;197m-in_lh\033[0m            : (required) full path to input data for left hemisphere. Can be .label.gii, .annot, .shape.gii, .curv or .txt
 \t\033[38;5;197m-in_rh\033[0m            : (required) full path to input data for right hemisphere. Can be .label.gii, .annot, .shape.gii, .curv or .txt
-\t\033[38;5;197m-out_name\033[0m         : (required) prefix for output files. Output will be in the form $wd/${out_name}_lh_${out_space}.*.gii $wd/${out_name}_rh_${out_space}.*.gii
+\t\033[38;5;197m-out_name\033[0m         : (required) prefix for output files. Output will be in the form {wd}/{out_name}_lh_{out_space}.*.gii {wd}/{out_name}_rh_{out_space}.*.gii
 \t\033[38;5;197m-interp\033[0m           : (optional) interpolation method, can be linear or nearest. Only works for .txt. Other types are forced to defaults (label.gii and .annot to nearest, shape.gii and .curv to linear)
 
 \t\033[38;5;197m-h|-help\033[0m          : Print help
