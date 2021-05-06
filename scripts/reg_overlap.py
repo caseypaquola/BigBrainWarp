f12b605288af7e3a2ba5eb79cab59e8071ce3e0e
caseypaquola <34452058+caseypaquola@users.noreply.github.com>
2021-05-06 17:43:25 +0200

    Update reg_overlap.py

--- scripts/reg_overlap.py
+++ scripts/reg_overlap.py
@@ -1,15 +1,21 @@
 # Import packages
 import sys
 import numpy
+import nibabel as nib
 
 # Define input arguments
 wd = str(sys.argv[1])
+template = str(sys.argv[2])
 
-# Load coordinates and calculate distance
-p1 = numpy.loadtxt(wd + "trans_coords.txt")
-p2 = numpy.loadtxt(wd + "set_coords.txt")
-dist = numpy.zeros(32)
-for f in range(0,32):
-    dist[f] = numpy.linalg.norm(p1[f]-p2[f])
+# load template and transformed image
+img1 = nib.load(template)
+img1_data = img1.get_fdata()
+img2 = nib.load(wd + "trans_seg.mnc")
+img2_data = img2.get_fdata()
 
-numpy.savetxt(wd + "af_dist.txt", dist)
+# Calculate DICE overlap
+dice = numpy.zeros(22)
+for f in range(1,22):
+    dice[f-1] = sum([img1_data==f] and [img2_data==f])/sum([img1_data==f] or [img2_data==f])
+
+numpy.savetxt(wd + "reg_dice.txt", dice)
