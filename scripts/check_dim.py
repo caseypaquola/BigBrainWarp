c9aa2a18fb14e4316210b1605afaed22d865995d
caseypaquola <34452058+caseypaquola@users.noreply.github.com>
2021-06-28 20:20:50 +0200

    overhaul fsaverage_to_bigbrain

--- scripts/check_dim.py
+++ scripts/check_dim.py
@@ -3,5 +3,8 @@ import sys
 
 # load data and print the dimensions
 input=str(sys.argv[1])
-data = nib.load(input)
-print(data.darrays[0].data.shape[0])
+data=nib.load(input)
+
+# get number of vertices and round to nearest thousand
+vert_k=round(data.darrays[0].data.shape[0], -3) 
+print(str.strip(str(vert_k),"0"))
