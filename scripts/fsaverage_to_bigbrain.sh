e4a4a71974196bbcbf9e1e68ee8548f4663b7a91
caseypaquola <34452058+caseypaquola@users.noreply.github.com>
2021-06-28 20:36:28 +0200

    overhaul of fsaverage_to_bigbrain

--- scripts/fsaverage_to_bigbrain.sh
+++ scripts/fsaverage_to_bigbrain.sh
@@ -5,67 +5,90 @@
 #
 # written by Casey Paquola @ MICA, MNI, 2021
 
-lhInput=$1 		# full path to left hemisphere input file
-rhInput=$2 		# full path to right hemisphere input file
-inSurf=$3		# input surface can be "fsaverage" or "fs_LR"
-outName=$4 		# full path of output file (without extension or hemisphere label, eg: User/BigBrain/tests/Ghist). 
+in_lh=$1 		# full path to left hemisphere input file
+in_rh=$2 		# full path to right hemisphere input file
+in_space=$3		# input surface can be "fsaverage" or "fs_LR"
+desc=$4 		# name of descriptor
+wd=$5			# working directory
 
-# the output takes the form ${outName}_${hemi}_${outSurf}.${giiType}.gii
-# where hemi is lh and rh, they are saved out separately
-# default type is shape, however, .annot and .label.gii files will be label type
+# the output takes the form:
+# ${wd}/tpl-bigbrain_hemi-L_desc-${desc}.${gii_type}.gii  
+# ${wd}/tpl-bigbrain_hemi-R_desc-${desc}.${gii_type}.gii
+#
+# default $gii_type is shape, however, .annot and .label.gii files will be label type
 
 # check for input data type
-filename=$(basename -- "$lhInput")
+filename=$(basename -- "$in_lh")
 extension="${filename##*.}"
 echo $extension " input"
 
-for hemi in lh rh ; do
+# check density of input surface
+in_den=`python check_dim.py $in_lh`
+
+for hemi in L R ; do
 	# define input
-	if [[ "$hemi" == "lh" ]] ; then
-		inData=$lhInput
+	if [[ "$hemi" == "L" ]] ; then
+		inData=$in_lh
 	else
-		inData=$rhInput
+		inData=$in_rh
 	fi
 
-	# rename inSurf to be able to find files
-	if [[ "$inSurf" == "fsaverage" ]] ; then
-		inSurf2="fsavg"
-	elif [[ "$inSurf" == "fs_LR" ]] ; then
-		inSurf2="fsLR"
-	fi
-
-
-	# define giiType and convert to gifti if necessary
+	# define gii_type and convert to gifti if necessary
 	if [[ "$extension" == "gii" ]] ; then
 		substr="${filename: -10}"
 		if [[ $substr == *"label"* ]] ; then
-			giiType=label
+			gii_type=label
 		else 
-			giiType=shape
+			gii_type=shape
 		fi
-		cp $inData ${outName}_${hemi}.${giiType}.gii
+		cp $inData ${wd}/tpl-${in_space}_hemi-${hemi}_den-${in_den}k_desc-${desc}.${gii_type}.gii
 	elif [[ "$extension" == "annot" ]] ; then
-		giiType=label
-		mris_convert --annot $inData $bbwDir/spaces/${inSurf}/${hemi}.${inSurf2}.white.surf.gii ${outName}_${hemi}.${giiType}.gii
+		gii_type=label
+		mris_convert --annot $inData \
+			$bbwDir/spaces/tpl-${in_space}/tpl-${in_space}_hemi-${hemi}_den-${in_den}k_desc-white.surf.gii \
+			${wd}/tpl-${in_space}_hemi-${hemi}_den-${in_den}k_desc-${desc}.${gii_type}.gii
 	elif [[ "$extension" == "curv" ]] ; then
-		giiType=shape
-		mris_convert -c $inData $bbwDir/spaces/${inSurf}/${hemi}.${inSurf2}.white.surf.gii ${outName}_${hemi}.${giiType}.gii
+		gii_type=shape
+		mris_convert -c $inData \
+			$bbwDir/spaces/tpl-${in_space}/tpl-${in_space}_hemi-${hemi}_den-${in_den}k_desc-white.surf.gii \
+			${wd}/tpl-${in_space}_hemi-${hemi}_den-${in_den}k_desc-${desc}.${gii_type}.gii
 	elif [[ "$extension" == "txt" ]] ; then
-		giiType=shape
-		python $bbwDir/scripts/txt2curv.py $inData ${outName}_${hemi}.curv
-		mris_convert -c ${outName}_${hemi}.curv $bbwDir/spaces/${inSurf}/${hemi}.${inSurf2}.white.surf.gii ${outName}_${hemi}.${giiType}.gii
+		gii_type=shape
+		python $bbwDir/scripts/txt2curv.py $inData ${wd}/tpl-${in_space}_hemi-${hemi}_den-${in_den}k_desc-${desc}.curv
+		mris_convert -c ${wd}/tpl-${in_space}_hemi-${hemi}_den-${in_den}k_desc-${desc}.curv \
+			$bbwDir/spaces/tpl-${in_space}/tpl-${in_space}_hemi-${hemi}_den-${in_den}k_desc-white.surf.gii \
+			${wd}/tpl-${in_space}_hemi-${hemi}_den-${in_den}k_desc-${desc}.${gii_type}.gii
 	else
-		echo "non-standard file type - trying as .curv"
-		giiType=shape
-		mris_convert -c $inData $bbwDir/spaces/${inSurf}/${hemi}.${inSurf2}.white.surf.gii ${outName}_${hemi}.${giiType}.gii
+		echo "file type of $inData not recognised"
+	fi
+
+	# internal upsampling, if necessary
+	if [[ "$in_den" == "32" ]] ; then
+		if [[ "$gii_type" == "shape" ]] ; then
+			wb_command -metric-resample ${wd}/tpl-${in_space}_hemi-${hemi}_den-32k_desc-${desc}.${gii_type}.gii \
+				$bbwDir/spaces/tpl-${in_space}/tpl-${in_space}_hemi-${hemi}_den-32k_desc-sphere.surf.gii \
+				$bbwDir/spaces/tpl-${in_space}/tpl-${in_space}_hemi-${hemi}_den-164k_desc-sphere.surf.gii \ 
+				BARYCENTRIC \
+				${wd}/tpl-${in_space}_hemi-${hemi}_den-164k_desc-${desc}.${gii_type}.gii
+		elif [[ "$gii_type" == "label" ]] ; then
+			wb_command -label-resample ${wd}/tpl-${in_space}_hemi-${hemi}_den-32k_desc-${desc}.${gii_type}.gii \
+				$bbwDir/spaces/tpl-${in_space}/tpl-${in_space}_hemi-${hemi}_den-32k_desc-sphere.surf.gii \
+				$bbwDir/spaces/tpl-${in_space}/tpl-${in_space}_hemi-${hemi}_den-164k_desc-sphere.surf.gii \ 
+				BARYCENTRIC \
+				${wd}/tpl-${in_space}_hemi-${hemi}_den-164k_desc-${desc}.${gii_type}.gii
+		fi
 	fi
 
 	# multimodal surface matching
-	msmMesh=$bbwDir/xfms/${hemi}.sphere_BigBrain_rsled_like_${inSurf2}.sphere.reg.surf.gii
-	inMesh=$bbwDir/xfms/${hemi}.BigBrain.rot.${inSurf2}.sphere.surf.gii
-	if [[ "$giiType" == "shape" ]] ; then
-		wb_command -metric-resample ${outName}_${hemi}.${giiType}.gii $msmMesh $inMesh BARYCENTRIC ${outName}_${hemi}_bigbrain.${giiType}.gii	
-	elif [[ "$giiType" == "label" ]] ; then
-		wb_command -label-resample ${outName}_${hemi}.${giiType}.gii $msmMesh $inMesh BARYCENTRIC ${outName}_${hemi}_bigbrain.${giiType}.gii	
+	msmMesh=$bbwDir/xfms/tpl-${in_space}/tpl-${in_space}_hemi-${hemi}_den-164k_desc-sphere_rsled_like_bigbrain.reg.surf.gii
+	inMesh=$bbwDir/xfms/tpl-bigbrain/tpl-bigbrain_hemi-${hemi}_desc-sphere_rot_${in_space}.surf.gii
+	if [[ "$gii_type" == "shape" ]] ; then
+		wb_command -metric-resample ${wd}/tpl-${in_space}_hemi-${hemi}_den-164k_desc-${desc}.${gii_type}.gii \
+		$msmMesh $inMesh BARYCENTRIC \
+		${wd}/tpl-bigbrain_hemi-${hemi}_desc-${desc}.${gii_type}.gii
+	elif [[ "$gii_type" == "label" ]] ; then
+		wb_command -label-resample ${wd}/tpl-${in_space}_hemi-${hemi}_den-164k_desc-${desc}.${gii_type}.gii \
+		$msmMesh $inMesh BARYCENTRIC \
+		${wd}/tpl-bigbrain_hemi-${hemi}_desc-${desc}.${gii_type}.gii
 	fi
 done
