b652e4cebcc38e20e44e7eb28d05d416742e98e0
caseypaquola <34452058+caseypaquola@users.noreply.github.com>
2021-06-29 09:32:29 +0200

    overhaul of icbm

--- scripts/bigbrainsurf_to_icbm.sh
+++ scripts/bigbrainsurf_to_icbm.sh
@@ -5,73 +5,90 @@
 #
 # written by Casey Paquola @ MICA, MNI, 2021
 
-lhInput=$1 		# full path to left hemisphere input file
-rhInput=$2 		# full path to right hemisphere input file
-outName=$3 		# full path of output file (without extension or hemisphere label, eg: User/BigBrain/tests/Ghist). 
+in_lh=$1 		# full path to left hemisphere input file
+in_rh=$2 		# full path to right hemisphere input file
+desc=$3 		# name of descriptor
+wd=$4			# working directory
 
-# the output takes the form ${outName}_${hemi}_${outSurf}.${giiType}.gii
-# where hemi is lh and rh, they are saved out separately
-# default type is shape, however, .annot and .label.gii files will be label type
+# the output takes the form:
+# ${wd}/tpl-icbm_hemi-L_desc-${desc}.${gii_type}.gii  
+# ${wd}/tpl-icbm_hemi-R_desc-${desc}.${gii_type}.gii
+#
+# default $gii_type is shape, however, .annot and .label.gii files will be label type
 
-# check for input data type
-filename=$(basename -- "$lhInput")
+filename=$(basename -- "$in_lh")
 extension="${filename##*.}"
 
-for hemi in lh rh ; do
+for hemi in L R ; do
 	# define input
-	if [[ "$hemi" == "lh" ]] ; then
-		inData=$lhInput
+	if [[ "$hemi" == "L" ]] ; then
+		inData=$in_lh
 	else
-		inData=$rhInput
+		inData=$in_rh
 	fi
 
-	# define giiType and convert to gifti if necessary
+	# define gii_type and convert to gifti if necessary
 	if [[ "$extension" == "gii" ]] ; then
 		substr="${filename: -10}"		
 		if [[ $substr == *"label"* ]] ; then
-			giiType=label
+			gii_type=label
 		else 
-			giiType=shape
+			gii_type=shape
 		fi
-		cp $inData ${outName}_${hemi}_${outSurf}.${giiType}.gii
+		cp $inData ${wd}/tpl-bigbrain_hemi-${hemi}_desc-${desc}.${gii_type}.gii
 	elif [[ "$extension" == "annot" ]] ; then
-		giiType=label
-		mris_convert --annot $inData $bbwDir/spaces/bigbrain/${hemi}.BigBrain.white.surf.gii ${outName}_${hemi}.${giiType}.gii
+		gii_type=label
+		mris_convert --annot $inData \
+			$bbwDir/spaces/tpl-bigbrain/tpl-bigbrain_hemi-${hemi}_desc-white.surf.gii \
+			${wd}/tpl-bigbrain_hemi-${hemi}_desc-${desc}.${gii_type}.gii
 	elif [[ "$extension" == "curv" ]] ; then
-		giiType=shape
-		mris_convert -c $inData $bbwDir/spaces/bigbrain/${hemi}.BigBrain.white.surf.gii ${outName}_${hemi}.${giiType}.gii
+		gii_type=shape
+		mris_convert -c $inData \
+			$bbwDir/spaces/tpl-bigbrain/tpl-bigbrain_hemi-${hemi}_desc-white.surf.gii \
+			${wd}/tpl-bigbrain_hemi-${hemi}_desc-${desc}.${gii_type}.gii
 	elif [[ "$extension" == "txt" ]] ; then
 		if [[ -z $interp ]] ; then
-			giiType=shape
+			gii_type=shape
 		elif [[  "$interp" == "linear" ]] ; then
-			giiType=shape
+			gii_type=shape
 		elif [[  "$interp" == "nearest" ]] ; then			
-			giiType=label
+			gii_type=label
 		fi
-		python $bbwDir/scripts/txt2curv.py $inData ${outName}_${hemi}.curv
-		mris_convert -c ${outName}_${hemi}.curv $bbwDir/spaces/bigbrain/${hemi}.BigBrain.white.surf.gii ${outName}_${hemi}.${giiType}.gii
+		python $bbwDir/scripts/txt2curv.py $inData ${wd}/tpl-bigbrain_hemi-${hemi}_desc-${desc}.curv
+		mris_convert -c ${wd}/tpl-bigbrain_hemi-${hemi}_desc-${desc}.curv \
+			$bbwDir/spaces/tpl-bigbrain/tpl-bigbrain_hemi-${hemi}_desc-white.surf.gii \
+			${wd}/tpl-bigbrain_hemi-${hemi}_desc-${desc}.${gii_type}.gii
 	fi
-    if [[ $hemi == "lh" ]] ; then
+
+	# gives struc labels to gifti
+    if [[ $hemi == "L" ]] ; then
         struc_label=CORTEX_LEFT
-	hemi_long=left
     else
         struc_label=CORTEX_RIGHT
-	hemi_long=right
     fi
+
 	# multimodal surface matching
-	refmesh=$bbwDir/xfms/$hemi.MNI152.rot.fsavg.sphere.surf.gii
-	outmeshMSM=$bbwDir/xfms/$hemi.sphere_MNI152_rsled_like_BigBrain.sphere.reg.surf.gii
-	if [[ "$giiType" == "shape" ]] ; then
-		wb_command -metric-resample ${outName}_${hemi}.${giiType}.gii $outmeshMSM $refmesh BARYCENTRIC ${outName}_${hemi}_icbm.${giiType}.gii
-		wb_command -set-structure ${outName}_${hemi}_icbm.${giiType}.gii $struc_label
-		wb_command -metric-to-volume-mapping ${outName}_${hemi}_icbm.${giiType}.gii $bbwDir/spaces/icbm/icbm_avg_mid_sym_mc_${hemi_long}_hires.surf.gii \
-		$bbwDir/spaces/icbm/mni_icbm152_t1_tal_nlin_sym_09c_mask.nii ${outName}_${hemi}_icbm.nii \
-		-ribbon-constrained $bbwDir/spaces/icbm/icbm_avg_mid_sym_mc_${hemi_long}_hires.surf.gii $bbwDir/spaces/icbm/icbm_avg_white_sym_mc_${hemi_long}_hires.surf.gii
-	elif [[ "$giiType" == "label" ]] ; then
-		wb_command -label-resample ${outName}_${hemi}.${giiType}.gii $outmeshMSM $refmesh BARYCENTRIC ${outName}_${hemi}_icbm.${giiType}.gii
-		wb_command -set-structure ${outName}_${hemi}_icbm.${giiType}.gii $struc_label
-		wb_command -label-to-volume-mapping ${outName}_${hemi}_icbm.${giiType}.gii $bbwDir/spaces/icbm/icbm_avg_mid_sym_mc_${hemi_long}_hires.surf.gii \
-		$bbwDir/spaces/icbm/mni_icbm152_t1_tal_nlin_sym_09c_mask.nii ${outName}_${hemi}_icbm.nii \
-		-ribbon-constrained $bbwDir/spaces/icbm/icbm_avg_mid_sym_mc_${hemi_long}_hires.surf.gii $bbwDir/spaces/icbm/icbm_avg_white_sym_mc_${hemi_long}_hires.surf.gii
+	refmesh=$bbwDir/spaces/tpl-icbm/tpl-icbm_hemi-${hemi}_desc-sphere_rot_fsaverage.surf.gii
+	outmeshMSM=$bbwDir/xfms/tpl-icbm_hemi-${hemi}_desc-sphere_rsled_like_bigbrain.reg.surf.gii
+	if [[ "$gii_type" == "shape" ]] ; then
+		wb_command -metric-resample ${wd}/tpl-bigbrain_hemi-${hemi}_desc-${desc}.${gii_type}.gii \
+			$outmeshMSM $refmesh BARYCENTRIC ${wd}/tpl-icbm_hemi-${hemi}_desc-${desc}.${gii_type}.gii
+		wb_command -set-structure ${wd}/tpl-icbm_hemi-${hemi}_desc-${desc}.${gii_type}.gii $struc_label
+		wb_command -metric-to-volume-mapping ${wd}/tpl-icbm_hemi-${hemi}_desc-${desc}.${gii_type}.gii \
+			$bbwDir/spaces/tpl-icbm/tpl-icbm_hemi-${hemi}_desc-white.surf.gii \
+			$bbwDir/spaces/tpl-icbm/tpl-icbm_desc-t1_tal_nlin_sym_09c_mask.nii \
+			${wd}/tpl-icbm_hemi-${hemi}_desc-${desc}.nii \
+			-ribbon-constrained $bbwDir/spaces/tpl-icbm/tpl-icbm_hemi-${hemi}_desc-mid.surf.gii \
+			$bbwDir/spaces/tpl-icbm/tpl-icbm_hemi-${hemi}_desc-white.surf.gii
+	elif [[ "$gii_type" == "label" ]] ; then
+		wb_command -label-resample ${wd}/tpl-bigbrain_hemi-${hemi}_desc-${desc}.${gii_type}.gii \
+			$outmeshMSM $refmesh BARYCENTRIC ${wd}/tpl-icbm_hemi-${hemi}_desc-${desc}.${gii_type}.gii
+		wb_command -set-structure ${wd}/tpl-icbm_hemi-${hemi}_desc-${desc}.${gii_type}.gii $struc_label
+		wb_command -label-to-volume-mapping ${wd}/tpl-icbm_hemi-${hemi}_desc-${desc}.${gii_type}.gii \
+			$bbwDir/spaces/tpl-icbm/tpl-icbm_hemi-${hemi}_desc-white.surf.gii \
+			$bbwDir/spaces/tpl-icbm/tpl-icbm_desc-t1_tal_nlin_sym_09c_mask.nii \
+			${wd}/tpl-icbm_hemi-${hemi}_desc-${desc}.nii \
+			-ribbon-constrained $bbwDir/spaces/tpl-icbm/tpl-icbm_hemi-${hemi}_desc-mid.surf.gii \
+			$bbwDir/spaces/tpl-icbm/tpl-icbm_hemi-${hemi}_desc-white.surf.gii
 	fi
 done
