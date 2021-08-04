625773279174afd55d073d8e19505001d86231ed
caseypaquola <34452058+caseypaquola@users.noreply.github.com>
2021-08-04 14:55:46 +0200

    Update demo_dockerbased.sh

--- scripts/demo_dockerbased.sh
+++ scripts/demo_dockerbased.sh
@@ -10,28 +10,28 @@ docker run -it --rm -v /local/directory/with/data/:/BigBrainWarp/tests \
 
 # FORM 2 - bigbrain volume to fsaverage
 docker run -it --rm -v /local/directory/with/data/:/BigBrainWarp/tests \
-        caseypaquola/bigbrainwarp bigbrainwarp \
+        caseypaquola/bigbrainwarp \
         bigbrainwarp --in_space bigbrainsym --in_vol "$bbwDir"/spaces/tpl-bigbrain/tpl-bigbrain_desc-cls_1000um_sym.nii \
         --interp linear --out_type surface --out_space fsaverage \
         --desc cls --wd "$wd"
 
 # FORM 3 - icbm volume to bigbrain volume
 docker run -it --rm -v /local/directory/with/data/:/BigBrainWarp/tests \
-        caseypaquola/bigbrainwarp bigbrainwarp \
+        caseypaquola/bigbrainwarp \
         bigbrainwarp --in_space icbm --in_vol "$bbwDir"/spaces/tpl-icbm/tpl-icbm_desc-t1_tal_nlin_sym_09c_mask.mnc \
         --interp nearest --out_space bigbrainsym --out_res 2 \
         --desc cls --wd "$wd"
 
 # FORM 4 - icbm volume to bigbrain surface
 docker run -it --rm -v /local/directory/with/data/:/BigBrainWarp/tests \
-        caseypaquola/bigbrainwarp bigbrainwarp \
+        caseypaquola/bigbrainwarp \
         bigbrainwarp --in_space icbm --in_vol "$bbwDir"/spaces/tpl-icbm/tpl-icbm_desc-t1_tal_nlin_sym_09c_mask.mnc \
         --interp nearest --out_space bigbrainsym --out_type surface \
         --desc cls --wd "$wd"
 
 # FORM 5 - bigbrain surface to icbm volume
 docker run -it --rm -v /local/directory/with/data/:/BigBrainWarp/tests \
-        caseypaquola/bigbrainwarp bigbrainwarp \
+        caseypaquola/bigbrainwarp \
         bigbrainwarp --in_space bigbrain \
         --in_lh "$bbwDir"/spaces/tpl-bigbrain/tpl-bigbrain_hemi-L_desc-Hist_G1.txt \
         --in_rh "$bbwDir"/spaces/tpl-bigbrain/tpl-bigbrain_hemi-R_desc-Hist_G1.txt \
@@ -40,7 +40,7 @@ docker run -it --rm -v /local/directory/with/data/:/BigBrainWarp/tests \
 
 # FORM 6 - bigbrain surface to fsaverage
 docker run -it --rm -v /local/directory/with/data/:/BigBrainWarp/tests \
-        caseypaquola/bigbrainwarp bigbrainwarp \
+        caseypaquola/bigbrainwarp \
         bigbrainwarp --in_space bigbrain \
         --in_lh "$bbwDir"/spaces/tpl-bigbrain/tpl-bigbrain_hemi-L_desc-Hist_G1.txt \
         --in_rh "$bbwDir"/spaces/tpl-bigbrain/tpl-bigbrain_hemi-R_desc-Hist_G1.txt \
@@ -49,7 +49,7 @@ docker run -it --rm -v /local/directory/with/data/:/BigBrainWarp/tests \
 
 # FORM 7 - fsaverage to bigbrain volume
 docker run -it --rm -v /local/directory/with/data/:/BigBrainWarp/tests \
-        caseypaquola/bigbrainwarp bigbrainwarp \
+        caseypaquola/bigbrainwarp \
         bigbrainwarp --in_space fsaverage \
         --in_lh "$bbwDir"/spaces/tpl-fsaverage/tpl-fsaverage_hemi-L_den-164k_desc-Func_G1.curv \
         --in_rh "$bbwDir"/spaces/tpl-fsaverage/tpl-fsaverage_hemi-R_den-164k_desc-Func_G1.curv \
@@ -58,7 +58,7 @@ docker run -it --rm -v /local/directory/with/data/:/BigBrainWarp/tests \
 
 # FORM 8 - fsaverage to bigbrain surface
 docker run -it --rm -v /local/directory/with/data/:/BigBrainWarp/tests \
-        caseypaquola/bigbrainwarp bigbrainwarp \
+        caseypaquola/bigbrainwarp \
         bigbrainwarp --in_space fsaverage \
         --in_lh "$bbwDir"/spaces/tpl-fsaverage/tpl-fsaverage_hemi-L_den-164k_desc-Func_G1.curv \
         --in_rh "$bbwDir"/spaces/tpl-fsaverage/tpl-fsaverage_hemi-R_den-164k_desc-Func_G1.curv \
