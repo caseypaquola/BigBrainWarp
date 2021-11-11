49df6854d889f505d159f24b7bbc3c454f925e99
caseypaquola <89579079+multiscale-neuroanatomy@users.noreply.github.com>
2021-11-11 09:05:19 +0100

    changing gitignore behaviour

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,11 +8,6 @@ export mnc2Path=/data_/mica1/01_programs/minc2/ # path to your path to minc2 ins
 export icbmTemplate=$bbwDir/spaces/tpl-icbm/tpl-icbm_desc-t1_tal_nlin_sym_09c_mask.mnc
 cd $bbwDir
 
-# make git ignore
-if [[ ! -f $bbwDir/.gitignore ]] ; then
-	cp $bbwDir/template_gitignore.txt $bbwDir/.gitignore
-fi
-
 # add to paths
 export PATH=$bbwDir:$bbwDir/scripts/:$mnc2Path/:$wbPath:$PATH
 export MATLABPATH=$bbwDir/scripts/:$MATLABPATH
