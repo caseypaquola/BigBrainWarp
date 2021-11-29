e5cf67baceaf7ee8f7e3080eb96333bc5bfbd5c3
caseypaquola <89579079+multiscale-neuroanatomy@users.noreply.github.com>
2021-11-29 16:40:34 +0100

    update downloads script

--- scripts/downloads.sh
+++ scripts/downloads.sh
@@ -3,5 +3,11 @@
 cd $bbwDir
 wget --no-check-certificate --content-disposition "https://fz-juelich.sciebo.de/s/pFu9XfNonT65HpS/download"
 unzip BigBrainWarp.zip
-
-# run checksum
+cd BigBrainWarp
+unzip spaces.zip
+mv spaces ../spaces
+unzip xfms.zip
+mv xfms ../xfms
+rm BigBrainWarp.zip
+rm spaces.zip
+rm xfms.zip
