a59f34061b07d19f0db3c7d1cf572287eeb6f8b9
caseypaquola <casey.paquola@gmail.com>
2022-01-19 16:38:32 +0100

    Update downloads.sh

--- scripts/downloads.sh
+++ scripts/downloads.sh
@@ -3,7 +3,7 @@
 cd $bbwDir
 curl https://fz-juelich.sciebo.de/s/pFu9XfNonT65HpS/download --output BBW_BigData.zip
 unzip BBW_BigData.zip
-mv BBW_BigData/spaces/spaces spaces
+mv BBW_BigData/spaces spaces
 cd BBW_BigData
 unzip xfms.zip
 mv xfms ../xfms
@@ -12,4 +12,3 @@ rm -rf BBW_BigData
 rm BBW_BigData.zip
 rm -rf xfms/xfms
 rm -rf spaces/spaces
-
