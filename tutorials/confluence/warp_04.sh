ca116e9f15db3511e9f870a4c3692837170bf0cc
caseypaquola <34452058+caseypaquola@users.noreply.github.com>
2022-08-04 08:51:58 +0200

    Update warp_04.sh

--- tutorials/confluence/warp_04.sh
+++ tutorials/confluence/warp_04.sh
@@ -2,9 +2,16 @@
 
 # Tutorial: Variations in resting state functional connectivity along a histological axis
 docker pull caseypaquola/bigbrainwarp:latest
-docker run -it --rm -v /local/directory/with/freesurfer_license:/license \
-        -v /local/directory/with/data/:/BigBrainWarp/tests \
-        bigbrainwarp --in_space bigbrain --out_space icbm --wd /BigBrainWarp/tests \
-        --desc confluence --interp trilinear \
-        --in /BigBrainWarp/tests/histological_axis_vox.nii 
+docker run -it --rm -v /local/directory/with/data/:/BigBrainWarp/mount \
+        caseypaquola/bigbrainwarp bigbrainwarp --in_space bigbrain --out_space icbm --wd /BigBrainWarp/mount \
+                --in /BigBrainWarp/tests/histological_axis_vox.nii \
+                --desc confluence \
+                --interp trilinear
+                
+# to run locally, from within BigBrainWarp github repo
+#bigbrainwarp --in_space bigbrain --out_space icbm --wd /local_directory/for/output/ \
+#                --in /full/path/to/histological_axis_vox.nii \
+#                --desc confluence \
+#                --interp trilinear
+
 
